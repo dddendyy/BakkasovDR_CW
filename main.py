96b71a8ee45b5ca8f2f95b1b39e4d9a8dce57577
dddendyy <dbokasin@yandex.ru>
2024-03-01 23:13:29 +0300

    fix print_operations_from()

--- main.py
+++ main.py
@@ -2,31 +2,48 @@ import json
 import datetime
 
 
-def print_operations_from(operation_from):
-    number = operation_from.split()[-1]
-    if len(number) == 20:
-        print(f"{number[:4]} {number[4:6]}** **** **** {number[-4:]}", end=' ')
+def print_bank_account(operation_from):
+    '''
+    Функция для конвертирования номера счёта отправителя по маске
+    1111 11** **** 1111 для карт (16-значные номера)
+    **1111 для счетов (20-значные номер)
+    '''
+    number = operation_from.split()[-1] # с помощью split получаем номер
 
-    else:
-        print(f"{number[:4]} {number[4:6]}** **** {number[-4:]}", end=' ')
+    if len(operation_from.split()) == 3: # если в строке 3 слова (например Visa Gold {номер}), то надо будет вывести полное название карты
+        if len(number) == 16: # если номер состоит из 16 символов
+            print(f"{operation_from.split()[0]} {operation_from.split()[1]}"
+                  f" {number[:4]} {number[4:6]}** **** **** {number[-4:]}", end=' ') # с помощью срезов добиваемся результата
+
+        else: # если номер состоит НЕ из 16 символов, то выводим по маске счёта
+            print(f"Счет **{number[-4:]}", end=' ')
+
+    else: # соответственно, если в строке всего два слова, то в названии только одно первое
+        if len(number) == 16:  # если номер состоит из 20 символов
+            print(f"{operation_from.split()[0]} {number[:4]} {number[4:6]}** **** **** {number[-4:]}",
+                  end=' ')  # с помощью срезов добиваемся результата
+
+        else:  # если номер состоит НЕ из 16 символов, то выводим по маске счёта
+            print(f"Счет **{number[-4:]}", end=' ')
 
 
 with open('operations.json', encoding='UTF-8') as file:
-    operations = json.load(file)
+    operations = json.load(file) # открываем файл и считываем значение
 
 
 for operation in operations:
-    if operation.get('date'):
-        if operation.get('from'):
-            print(f"{operation['date']} {operation['description']}")
-            print_operations_from(operation['from'])
-            print(f"-> {operation['to']}")
+    if operation.get('id'): # проверяем существование операции (мы не попадёмся на ваши пустые записи {} !!!)
+        if operation.get('from'): # отправителя может и не быть, т.к. есть операции по открытию счетов
+            print(f"{operation['date']} {operation['description']}") # ну а дальше вывод
+            print_bank_account(operation['from'])
+            print("->", end=' ')
+            print_bank_account(operation['to'])
             print(f"{operation['operationAmount']['amount']} {operation['operationAmount']['currency']['name']}\n")
 
         else:
-            print(f"{operation['date']} {operation['description']}\n"
-                  f"{operation['to']}\n"
-                  f"{operation['operationAmount']['amount']} {operation['operationAmount']['currency']['name']}")
+            print(f"{operation['date']} {operation['description']}")
+            print_bank_account(operation['to'])
+            print(f"{operation['operationAmount']['amount']} {operation['operationAmount']['currency']['name']}\n")
 
 
 
