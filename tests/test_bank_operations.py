0bdc40d94c7da6837e03ee379a29414cf9b00f69
dddendyy <dbokasin@yandex.ru>
2024-03-03 21:31:03 +0300

    add .coverage file

--- tests/test_bank_operations.py
+++ tests/test_bank_operations.py
@@ -5,4 +5,4 @@ def test_formatted_bank_account():
     assert formatted_bank_account('Счет 59956820797131895975') == 'Счет **5975'
     assert formatted_bank_account('МИР 8665240839126074') == 'МИР 8665 24** **** 6074'
     assert formatted_bank_account('Visa Classic 2842878893689012') == 'Visa Classic 2842 87** **** 9012'
-    # assert formatted_bank_account('Maestro 1596837868705199') == 'Maestro 1596 83** **** 5199'
+    assert formatted_bank_account('Maestro 1596837868705199') == 'Maestro 1596 83** **** 5199'
